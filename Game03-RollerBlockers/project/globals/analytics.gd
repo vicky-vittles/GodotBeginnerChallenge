69ca1510b2b4dfb7dc006839d535b2c228c93540
vitor.marques <vitor.marques@gatec.com.br>
2021-06-08 20:00:58 -0300

    Finished game 03

--- Game03-RollerBlockers/project/globals/analytics.gd
+++ Game03-RollerBlockers/project/globals/analytics.gd
@@ -1,20 +1,68 @@
 extends Node
 
+#const API_URL = "http://localhost:3000/"
+const API_URL = "https://player-analytics-api.herokuapp.com/"
+const LIST_GAMES = "games/"
+const CREATE_PLAY_SESSION = "play_sessions/create"
+const CREATE_SESSION_DATA = "session_data/create"
+
+const GAME_ID = "8721e12d-5f8b-4a4e-b9d7-8cad2d0855f6"
+const DEFAULT_NAME = "default"
+
+var debug_mode : bool = false
+var sending_analytics : bool = false
+var app_timer : float = 0.0
 var data = {
-	"total_playtime": 0, #In seconds
-	"level_playtimes": {
-		1: 0,
-		2: 0,
-		3: 0
-	},
-	"total_deaths": 0,
-	"level_deaths": {
-		1: 0,
-		2: 0,
-		3: 0
-	}
+	"total_playtime": 0 #In seconds
 }
 
+
+func _ready():
+	get_tree().set_auto_accept_quit(false)
+
+func _process(delta):
+	if Input.is_action_just_pressed("debug_mode"):
+		debug_mode = !debug_mode
+		print("Debug mode now: %s" % ["on" if debug_mode else "off"])
+	app_timer += delta
+	set_total_playtime(int(app_timer))
+
+func _notification(what):
+	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
+		if not debug_mode:
+			if not sending_analytics:
+				#print("Sending analytics...")
+				sending_analytics = true
+				send_data()
+		else:
+			get_tree().quit()
+
+
+# Send data
+func send_data():
+	var body = {
+		"data_json": data,
+		"name": DEFAULT_NAME,
+		"game_id": GAME_ID
+	}
+	var json_body = to_json(body)
+	var HEADERS = ["User-Agent: godot", "Content-Type: application/json"]
+	get_node("HTTP").request(API_URL+CREATE_SESSION_DATA, HEADERS, false, HTTPClient.METHOD_POST, json_body)
+
+func _on_request_completed(result, response_code, headers, body):
+	if response_code == 200:
+		var result_json = JSON.parse(body.get_string_from_utf8()).result
+		var result_data = result_json["data"]
+		
+		#print("HTTP Connection success\n")
+		#print(result_json)
+	else:
+		#print("HTTP Connection error %s \n" % response_code)
+		pass
+	get_tree().quit()
+
+
+# Set data
 func set_total_playtime(value: int):
 	data["total_playtime"] = value
 
--- Game03-RollerBlockers/project/main/game_quitter.gd
+++ Game03-RollerBlockers/project/main/game_quitter.gd
@@ -2,4 +2,4 @@ extends Node
 class_name GameQuitter
 
 func quit():
-	get_tree().quit()
+	get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
