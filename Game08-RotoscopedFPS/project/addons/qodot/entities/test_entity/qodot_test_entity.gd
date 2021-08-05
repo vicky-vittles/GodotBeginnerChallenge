ceef9501e85b40c3ff1a4c15cd8a65e908dcc813
vitor.marques <vitor.marques@gatec.com.br>
2021-08-05 19:35:49 -0300

    Reinstalling Qodot

--- Game08-RotoscopedFPS/project/addons/qodot/entities/test_entity/qodot_test_entity.gd
+++
@@ -1,2 +0,0 @@
-extends QodotFGDBaseClass
-class_name QodotTestEntity
--- Game08-RotoscopedFPS/project/levels/__level.gd
+++ Game08-RotoscopedFPS/project/levels/__level.gd
@@ -3,10 +3,10 @@ extends Spatial
 const TERRAIN_COLLISION_LAYER = 2
 const TERRAIN_COLLISION_MASK = 4
 
-onready var root = $root
-
 func _ready():
-	for entity in root.get_children():
-		if entity is StaticBody:
-			entity.collision_layer = 2
-			entity.collision_mask = 4
+	if has_node("root"):
+		var root = get_node("root")
+		for entity in root.get_children():
+			if entity is StaticBody:
+				entity.collision_layer = 2
+				entity.collision_mask = 4
