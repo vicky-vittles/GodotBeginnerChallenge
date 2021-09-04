7394f36c984480413ec6eaf1f370a7d3a661af53
Vitordavi-marques <vitordavi.marques@gmail.com>
2021-09-04 19:52:38 -0300

    Game 02 | Flail | Final version

--- CicloDesign/Game02-Flail/project/entities/entity_player/states/__entity_player_state.gd
+++ CicloDesign/Game02-Flail/project/entities/entity_player/states/__entity_player_state.gd
@@ -1,6 +1,7 @@
 extends GTState
 
 func physics_process(delta):
+	actor.input_controller.poll_input()
 	var move_direction = actor.input_controller.move_direction
 	actor.entity_mover.set_move_direction(move_direction)
 	actor.entity_mover.move(delta)
--- CicloDesign/Game02-Flail/project/menus/screens/screen_game_over.gd
+++ CicloDesign/Game02-Flail/project/menus/screens/screen_game_over.gd
@@ -6,7 +6,6 @@ signal button_quit()
 onready var score_label = $vbox/hbox/vbox/VBoxContainer/score_label
 
 func load_score():
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	var score = Globals.current_score
 	score_label.apply_text_simple(score)
 
