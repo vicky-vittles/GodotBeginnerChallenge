3f3630bfa395e5524639b4ae8ebb000d3be06d2c
Vitordavi-marques <vitordavi.marques@gmail.com>
2021-09-21 18:21:42 -0300

    Game 04 | Incremental Game | Removed game

--- CicloDesign/Game04-IncrementalGame/project/main/game/world/components/world_inventory.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-signal resource_points_updated()
-
-func _ready():
-	emit_signal("resource_points_updated")
-
-func _on_points_updated(current):
-	emit_signal("resource_points_updated")
--- CicloDesign/Game04-IncrementalGame/project/main/game/world/world.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready():
-	TranslationServer.set_locale("pt")
-
-func _on_DebugTimer_timeout():
-	pass
